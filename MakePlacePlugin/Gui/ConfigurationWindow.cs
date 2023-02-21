161c8ff06a5bd4986377817be2b61558a892e0e7
Drakansoul <71153307+Drakansoul@users.noreply.github.com>
2023-02-21 14:05:06 -0800

    enabled layout copying

--- MakePlacePlugin/Gui/ConfigurationWindow.cs
+++ MakePlacePlugin/Gui/ConfigurationWindow.cs
@@ -247,7 +247,7 @@ namespace MakePlacePlugin.Gui
 
             if (ImGui.Button($"Get {inOut} Layout"))
             {
-                if (IsDecorMode())
+                if (true)
                 {
                     try
                     {
